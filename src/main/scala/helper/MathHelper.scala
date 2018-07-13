4fead898bf67a416ba90c9bff5cc87722b8c2852
Théophile Champion <theophile.champion@epitech.eu>
2018-07-13 14:57:47 +0200

    Add answer of problem 004

--- src/main/scala/helper/MathHelper.scala
+++ src/main/scala/helper/MathHelper.scala
@@ -2,4 +2,5 @@ package helper
 
 object MathHelper {
   def is_prime(n:Long):Boolean = ! ((2L until n) exists (n % _ == 0L))
+  def is_palindrome(n:Long):Boolean = n.toString == n.toString.reverse
 }
---
+++ src/main/scala/problem_004.scala
@@ -0,0 +1,16 @@
+import helper.MathHelper
+
+object problem_004 extends App {
+  def check_product_of_two_3_digit(i:Int):Unit = {
+    for (n1 <- 999 to 100 by -1)
+      if (i % n1 == 0 && s"${i / n1}".toString.length == 3)
+        throw new Exception(s"Result = [$n1 ${i / n1}]")
+  }
+  try {
+    for (i <- 999 * 999 to 0 by -1)
+      if (MathHelper.is_palindrome(i))
+        check_product_of_two_3_digit(i)
+  } catch {
+    case e:Exception => println(e.getMessage)
+  }
+}
