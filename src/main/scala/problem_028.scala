6b8990cccea9e9d0947db4e26b0da993dba557a7
Théophile Champion <theophile.champion@epitech.eu>
2018-07-22 23:28:42 +0200

    Add answer of problem 027 and 028

---
+++ src/main/scala/problem_028.scala
@@ -0,0 +1,10 @@
+object problem_028 extends App {
+  var result = 1
+  var state = (3, 5, 7, 9)
+  for (i <- 4 to 1000 by 2) {
+    result += state._1 + state._2 + state._3 + state._4
+    state = (state._4 + i, state._4 + 2 * i, state._4 + 3 * i, state._4 + 4 * i)
+  }
+  result += state._1 + state._2 + state._3 + state._4
+  println(s"Result = $result")
+}
