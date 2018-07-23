26bc9bf4e4699dd7fb5bbf5dd4d28c76d8459446
Théophile Champion <theophile.champion@epitech.eu>
2018-07-23 20:10:25 +0200

    Add answer of problem 032

---
+++ src/main/scala/problem_032.scala
@@ -0,0 +1,23 @@
+import helper.MathHelper
+
+object problem_032 extends App {
+  val permutations = MathHelper.get_all_permutations("123456789")
+  var valid_answers = Array[(Long, Long)]()
+  for (p <- permutations) {
+    val l = p.length
+    for (i <- 1 to p.length - 2) {
+      for (j <- i + 1 until p.length) {
+        val n1 = p.slice(0, i).toLong
+        val n2 = p.slice(i, j).toLong
+        val n3 = p.slice(j, l).toLong
+        if (n1 * n2 == n3) valid_answers ++= Array[(Long, Long)]((n1, n2))
+      }
+    }
+  }
+  var result = valid_answers
+    .map{case(n1, n2) => if (n1 < n2) (n1, n2) else (n2, n1)}
+    .map{case(n1, n2) => n1 * n2}
+    .distinct
+    .sum
+  println(s"Result = $result")
+}
