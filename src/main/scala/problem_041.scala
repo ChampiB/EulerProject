8d569c4d8581ae1b05ecf9a4c89ef8a8a7c79d21
Théophile Champion <theophile.champion@epitech.eu>
2018-07-25 22:52:26 +0200

    Add answer of problem 41

---
+++ src/main/scala/problem_041.scala
@@ -0,0 +1,22 @@
+import helper.{InfiniteNumbersHelper, MathHelper}
+
+object problem_041 extends App {
+  def run():String = {
+    val s = "123456789"
+    val results = for (it <- 1 to s.length) yield {
+      val permutations = MathHelper.get_all_permutations(s.substring(0, it))
+      var again = true
+      var i = permutations.length - 1
+      while (i != -1 && again) {
+        if (MathHelper.is_prime(permutations(i).toLong)) {
+          again = false
+        } else {
+          i -= 1
+        }
+      }
+      if (i == -1) "0" else permutations(i)
+    }
+    results.map(_.toLong).max.toString
+  }
+  println(s"Result = ${run()}")
+}
---
+++ src/test/scala/problem_041_Test.scala
@@ -0,0 +1,7 @@
+import org.scalatest.FunSuite
+
+class problem_041_Test extends FunSuite {
+  test("problem_041.run") {
+    assert(problem_041.run() == "7652413")
+  }
+}
