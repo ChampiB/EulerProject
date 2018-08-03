f599d7bfa18d62d501ffef0af67a78e502f55898
Théophile Champion <theophile.champion@epitech.eu>
2018-08-03 19:12:13 +0200

    Add answer of problem 050

---
+++ src/main/scala/problem_050.scala
@@ -0,0 +1,34 @@
+import helper.MathHelper
+
+object problem_050 extends App {
+  def get_sum_of_consecutive_primes(primes:Array[Long], i:Int, length:Int):Long = {
+    val consecutive_primes = for (index <- i until i + length) yield primes(index)
+    consecutive_primes.sum
+  }
+  def run():String = {
+    val limit = 1000000
+    val primes = MathHelper.get_primes_bellow(limit)
+      .zipWithIndex
+      .filter{case(is_prime, _) => is_prime}
+      .map{case(_, index) => index + 1L}
+    var sum_length = primes.length
+    var again = true
+    var res = ""
+    while (again) {
+      var i = 0
+      var sum = get_sum_of_consecutive_primes(primes, i, sum_length)
+      while (again && sum_length + i < primes.length) {
+        if (sum < limit && primes.contains(sum)) {
+          again = false
+          res = sum.toString
+        }
+        if (sum_length + i < primes.length)
+          sum = sum - primes(i) + primes(i + sum_length)
+        i += 1
+      }
+      sum_length -= 1
+    }
+    s"$res"
+  }
+  println(s"Result = ${run()}")
+}
---
+++ src/test/scala/problem_050_Test.scala
@@ -0,0 +1,7 @@
+import org.scalatest.FunSuite
+
+class problem_050_Test extends FunSuite {
+  test("problem_050.run") {
+    assert(problem_050.run() == "997651")
+  }
+}
